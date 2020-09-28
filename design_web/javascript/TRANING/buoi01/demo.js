251f2cedca66a71935b7369e0029517719411af2
Ngọc Nguyễn <ngocnk99@gmail.com>
2020-09-28 22:07:33 +0700

    bat dau thuc tap

--- design_web/javascript/TRANING/buoi01/demo.js
+++ design_web/javascript/TRANING/buoi01/demo.js
@@ -1,27 +1,31 @@
-let name = 'mtp';
-let obj = {
-    name: 'son tung',
-    start: function(){
-        console.log(this.name)
-    }
-}
-console.log("thu voi function")
-obj.start();
-let a = obj.start;
-a();
-a.apply(obj);
+// let name = 'mtp';
+// let obj = {
+//     name: 'son tung',
+//     start: function(){
+//         console.log(this.name)
+//     }
+// }
+// console.log("thu voi function")
+// obj.start();
+// let a = obj.start;
+// a();
+// a.apply(obj);
 
 
-/* -------------------------------*/
-let obj2 = {
-    name: 'son tung',
-    start: () =>{
-        console.log(this.name)
-    }
-}
+// /* -------------------------------*/
+// let obj2 = {
+//     name: 'son tung',
+//     start: () =>{
+//         console.log(this.name)
+//     }
+// }
 
-console.log("\n thu voi => ")
-obj2.start();
-let b = obj2.start;
-b();
-b.apply(obj2);
+// console.log("\n thu voi => ")
+// obj2.start();
+// let b = obj2.start;
+// b();
+// b.apply(obj2);
+
+let c = 1;
+ c = 2;
+console.log(c);
